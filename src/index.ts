5481d5a1768a0ac0344660f92f5f15f8167319e6
うさみょん/myon2019 <56076195+mtripg6666tdr@users.noreply.github.com>
2026-02-15 02:42:32 +0900

    fix: ffmpeg robustness

--- src/index.ts
+++ src/index.ts
@@ -134,7 +134,13 @@ async function baseSpawnFFmpeg<T extends keyof BaseSpawnFFmpegTypeMap>(binary: s
       args.push(filename);
     }
 
-    const ffmpeg = spawn(binary, args, { stdio: ["ignore", "pipe", "pipe"] });
+    const ffmpeg = spawn(binary, [
+      "-reconnect", "1",
+      "-reconnect_at_eof", "1",
+      "-reconnect_streamed", "1",
+      "-reconnect_delay_max", "5",
+      ...args
+    ], { stdio: ["ignore", "pipe", "pipe"] });
 
     ffmpeg.stderr.pipe(process.stderr);
 
